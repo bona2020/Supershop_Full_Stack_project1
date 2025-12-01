a9306d1d8388f8c81674084f13c8d815e0b8d3af
bona2020 <bona@infonix.net>
2025-12-02 00:17:22 +0530

    now

--- backend/utils.py
+++ backend/utils.py
@@ -1,11 +1,13 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+# from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 def get_conn():
-    db_url = os.getenv('database_link')
-    conn = psycopg2.connect(db_url,cursor_factory=RealDictCursor)
+    db_url = os.getenv("DATABASE_URL")
+    conn = psycopg2.connect(db_url)
     return conn
